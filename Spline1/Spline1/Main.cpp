8f8f81dcdc935a8d7455c800e58ea3682f237c5a
Ilyuza <Gizyaschwann@gmail.com>
2018-02-21 11:08:38 +0300

    Working version with correct answers

--- Spline1/Spline1/Main.cpp
+++ Spline1/Spline1/Main.cpp
@@ -12,56 +12,57 @@ ifstream fin("input.in");
 ofstream fout("output.out");
 
 int n, m, k;
-vector<double>x_values;
-vector<vector<double>>y_values;
-vector<double>points;
-vector<vector<double>>coefficients;
-vector<int>indexes;
-double alph, l, u, z, c, b, d;
+vector<long double>x_values;
+vector<vector<long double>>y_values;
+vector<long double>points;
+vector<vector<long double>>coefficients;
+vector<long int>indexes;
+long double alph, l, u, z, c, b, d;
 // vector<double>::iterator low, up;
 const int COEF = 10; // 10 - x, y, h, alph, l, u, z, c, b, d;
 
 int main() {
-	fin >> n;
+	cin >> n;
 	x_values.resize(n);
-	coefficients.resize(n, vector<double>(COEF));
+	coefficients.resize(n, vector<long double>(COEF));
 
-	double temp;
+	long double temp;
 	for (int i = 0; i < n; i++)
 	{
-		fin >> temp;
+		cin >> temp;
 		x_values[i] = temp;
 		coefficients[i][0] = temp;
 	}
 
-	fin >> m;
-	y_values.resize(m, vector<double>(n));
+	cin >> m;
+	y_values.resize(m, vector<long double>(n));
 
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++)
 		{
-			fin >> temp;
+			cin >> temp;
 			y_values[i][j] = temp;
+			fout << temp << " ";
 		}
+		fout << endl;
+	}
 
-	fin >> k;
+	cin >> k;
 	//cout << k << " - this is k" << endl;
 	points.resize(k);
 	indexes.resize(k);
 	for (int i = 0; i < k; i++)
 	{
-		fin >> temp;
+		cin >> temp;
 		points[i] = temp;
-		auto up = upper_bound(x_values.begin(), x_values.end() - 1, temp);
-		//cout << *up << " - YEEEEEY" << endl;
-		auto idx = up - x_values.begin();
-		int index = (int)idx;
-		/*cout << index << "- INDEX" << endl;
-		cout << x_values[i] << " " << temp << endl;*/
-		indexes[i] = index - 1;
 	}
 
+
 	// filling l, u, z, c, b ,d const values on border points
+
+	coefficients[n - 1][3] = -1;
+	coefficients[0][3] = -1;
+	coefficients[n - 1][3] = -1;
 	coefficients[0][4] = 1;
 	coefficients[n - 1][4] = 1;
 	coefficients[0][5] = 0;
@@ -85,14 +86,13 @@ int main() {
 		// filling h values
 		for (int i = 0; i < n - 1; i++) {
 			coefficients[i][2] = x_values[i + 1] - x_values[i];
-			coefficients[i][2] = x_values[i + 1] - x_values[i];
+			
 		}
 		coefficients[n - 1][2] = -1;
 
 		// filling alph values
 		coefficients[0][3] = -1;
 		coefficients[n - 1][3] = -1;
-
 
 		for (int i = 1; i < n - 1; i++) {
 			coefficients[i][3] = (3 * (coefficients[i + 1][1] - coefficients[i][1]) / coefficients[i][2]) - (3 * (coefficients[i][1] - coefficients[i - 1][1]) / coefficients[i - 1][2]);
@@ -103,31 +103,47 @@ int main() {
 		for (int i = 1; i < n - 1; i++) {
 			coefficients[i][4] = 2 * (coefficients[i + 1][0] - coefficients[i - 1][0]) - coefficients[i - 1][2] * coefficients[i - 1][5];
 			coefficients[i][5] = coefficients[i][2] / coefficients[i][4];
-			coefficients[i][6] = (coefficients[i][1] - coefficients[i - 1][2] * coefficients[i - 1][6]);
+			coefficients[i][6] = (coefficients[i][3] - coefficients[i - 1][2] * coefficients[i - 1][6])/coefficients[i][4];
 		}
 
+
 		// filling c, b, d
-		for (int i = n - 2; i > 0; i--)
+		for (int i = n - 2; i >= 0; i--)
 		{
 			coefficients[i][7] = (coefficients[i][6] - coefficients[i][5] * coefficients[i + 1][7]);
-			coefficients[i][8] = (coefficients[i + 1][1] - coefficients[i][1]) / coefficients[i][2] - coefficients[i][2] * (coefficients[i + 1][7] + 2 * coefficients[i][7]) / 3;
+			coefficients[i][8] = ((coefficients[i + 1][1] - coefficients[i][1]) / coefficients[i][2]) - (coefficients[i][2] * (coefficients[i + 1][7] + 2 * coefficients[i][7]) / 3);
 			coefficients[i][9] = (coefficients[i + 1][7] - coefficients[i][7]) / (3 * coefficients[i][2]);
 		}
 		coefficients[0][7] = (coefficients[0][6] - coefficients[0][5] * coefficients[1][7]);
 
+		fout << endl;
+		for (int i = 0; i < n; i++) {
+			fout << setprecision(16) << " x is: " << coefficients[i][0] << " y is: " << coefficients[i][1] << " h is: " << coefficients[i][2] << " alph is: " << coefficients[i][3] << " l is: " << coefficients[i][4] << " u is: " << coefficients[i][5] << " z is: " << coefficients[i][6] <<
+				" c is - " << coefficients[i][7] << " b is: " << coefficients[i][8] << " d is " << coefficients[i][9] << endl;
+		}
+
 		// finish completeion of y's set. Let's go to fiding values at control points
 
+		int index_temp;
 		for (int i = 0; i < k; i++)
 		{
-			int c = indexes[i];
-			//fout << c << endl;
-			double difference = points[i] - coefficients[c][0];
-			fout << difference << endl;
-			double res = coefficients[c][1] + coefficients[c][9] * difference +
-				coefficients[c][7] * pow(difference, 2) + coefficients[c][9] * pow(difference, 3);
-			cout << setprecision(7) << res << " FUCCCCCCK YEEEEEEAAAH,    ";
+			auto up = upper_bound(x_values.begin(), x_values.end() - 1, points[i]);
+			//cout << *up << " - YEEEEEY" << endl;
+			auto idx = up - x_values.begin();
+			int index_temp = (int)idx;
+			/*cout << index_temp << "- index_temp" << endl;
+			cout << x_values[i] << " " << temp << endl;*/
+			index_temp--;
+
+			cout << index_temp << endl;
+			double difference = points[i] - coefficients[index_temp][0];
+			//fout << difference << endl;
+			fout << " This is point " << points[i] << " This is index " << index_temp << " This is difference " << difference<< endl;
+			double res = coefficients[index_temp][1] + coefficients[index_temp][8] * difference +
+				coefficients[index_temp][7] * pow(difference, 2) + coefficients[index_temp][9] * pow(difference, 3);
+			fout <<"\n"<< setprecision(16) << res << "YEEEEEEAAAH,    ";
 		}
-		cout << endl << "NEW LINE: " << endl;
+		cout << endl << "NEW LINE: \n";
 
 	}
 
